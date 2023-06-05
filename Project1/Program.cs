49447c314d56604b429be8eeac5603b973ebb6c5
salt4 <125229853+quocnguyen404@users.noreply.github.com>
2023-06-05 19:43:28 +0700

    Update Program.cs

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,39 +8,13 @@ namespace Project1
 {
     public class Program
     {
-        //Initialization UI
-        public static GameUIManager gameUIManager = new GameUIManager();
-        //Initialization UI
-
         public static void Main(string[] args)
         {
             while (true)
             {
-                gameUIManager.ShowBaseMenu();
+                GameUIManager.Instance.ShowBaseMenu();
             }
-            
         }
-
-        //Switch menu method
-        public static void WeaponMenu()
-        {
-            gameUIManager.weapon.MenuUI();
-        }
-
-        public static void ClothMenu()
-        {
-            gameUIManager.cloth.MenuUI();
-        }
-
-        public static void InventoryMenu()
-        {
-            gameUIManager.item.MenuUI();
-        }
-
-        public static void MainMenu()
-        {
-            gameUIManager.menu.MenuUI();
-        }
-        //Switch menu method
+        
     }
 }
